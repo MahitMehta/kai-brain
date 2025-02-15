491ebd99a4ce0eab84d5d244e80b78066447c7ee
Mahit Mehta <mahit.py@gmail.com>
2025-02-15 16:52:24 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::thread;
+
+fn main() {
+    println!("[KAI Brain] Hi (Ominously)");
+    
+    for i in 0..16 {
+        println!("[KAI Brain] Loading Brain Segment: 0x{:x}", i);
+        thread::sleep(std::time::Duration::from_millis(500));
+    }
+
+    println!("[KAI Brain] How may I serve you?");
+}
